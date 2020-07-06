44dc1200a9cad3a8b40597170eccf6f172b8ccfe
MBurnette4120 <mikeburnette@rocketmail.com>
2020-07-06 12:58:45 -0400

    updatedversion

---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public class Motorcycle : Vehicle
+    {
+        public Motorcycle()
+        {
+        }
+        public bool HasHandleBars { get; set; } = true;
+
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("This motorcycle is in drive.");
+        }
+
+        public override void DriveVirtual()
+        {
+            Console.WriteLine("This motorcycle is vehicle that is virtually in drive");
+        }
+
+
+
+
+
+    }
+}
